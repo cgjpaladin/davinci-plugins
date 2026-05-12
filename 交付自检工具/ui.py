f9c741275f292c087e9a6eca4011a50ea8ce8163
Bryan <bryan@BryandeMac-mini.local>
2026-05-12 22:03:57 +0800

    build_local: 交付自检工具 (from 9ca56fd)

--- 交付自检工具/ui.py
+++ 交付自检工具/ui.py
@@ -663,7 +663,6 @@ window_layout = [
         ]),  # 结束上半区 HGroup
 
         # ── 结果区：分组 + 数据 ──
-        ui.VGap({"Weight": 0, "MinimumSize": [0, 5]}),
         ui.HGroup({"Spacing": 4, "Weight": 1.0}, [
             ui.Tree({"ID": GROUP_TREE, "Weight": 0,
                      "Events": {"ItemClicked": True},
