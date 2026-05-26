ac2e0881505d521bae4f8aafd78fe4a354674ef8
Bryan <bryan@BryandeMac-mini.local>
2026-05-27 04:41:56 +0800

    build(createone): 04:41

--- 批量命名工具_创壹特供版/renamer_web.py
+++ 批量命名工具_创壹特供版/renamer_web.py
@@ -116,7 +116,7 @@ class RenamerAPI:
             elif nm == "V":     fmt.append({"pfx":"V","key":"ver"})
             elif k == "status": fmt.append({"pfx":"","key":"status"})
             else:               fmt.append({"pfx":"","key":k})
-        is_dev = not getattr(sys, 'frozen', False)
+        is_dev = not (getattr(sys, 'frozen', False) or bool(getattr(sys, '_MEIPASS', '')))
         return {
             "fields": FIELD_CONFIG,
             "dev": is_dev,
